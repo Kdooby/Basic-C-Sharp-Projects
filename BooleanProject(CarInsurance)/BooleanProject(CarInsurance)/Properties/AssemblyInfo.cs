c151b16352e4c8842b3da8b9fd125b24ca3a65f3
Kdooby <kevinmdem@gmail.com>
2021-02-11 14:45:40 -0800

    Resubmit Boolean Assignment

--- BooleanProject(CarInsurance)/BooleanProject(CarInsurance)/Properties/AssemblyInfo.cs
+++ BooleanProject(CarInsurance)/BooleanProject(CarInsurance)/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BooleanLogic")]
+[assembly: AssemblyTitle("BooleanProject(CarInsurance)")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BooleanLogic")]
+[assembly: AssemblyProduct("BooleanProject(CarInsurance)")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("77f13fc2-0a13-4932-884b-312083749190")]
+[assembly: Guid("c85eccfc-a193-4ca9-a7b7-143c2ef8f911")]
 
 // Version information for an assembly consists of the following four values:
 //
