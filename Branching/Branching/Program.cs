ea33fe129a64b339a0843257a960f01c5d850306
Kdooby <kevinmdem@gmail.com>
2021-02-11 19:02:26 -0800

    Push Branching Assignment
    simple program to evaluate shipping quote

---
+++ Branching/Branching/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Package Express.  Please follow the instructions below");
+            Console.ReadLine();
+
+            Console.WriteLine("Enter Package Weight: ");
+            decimal packageWeight = Convert.ToDecimal(Console.ReadLine());
+
+            if (packageWeight <= 50)
+            {
+                Console.WriteLine();
+            }
+
+            else
+            {
+                Console.WriteLine("Package is to heavy to be shipped with Package Express. Goodbye");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            
+
+            Console.WriteLine("Enter Package Width: ");
+            decimal packageWidth = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Enter Package Height: ");
+            decimal packageHeight = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Enter Package Length: ");
+            decimal packageLength = Convert.ToDecimal(Console.ReadLine());
+
+            decimal dimensions = packageWidth + packageHeight + packageLength;
+
+
+            if (dimensions > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express. Goodbye");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+            else
+            {
+                Console.WriteLine("Shipping Quote: ");
+                decimal quote = dimensions * packageWeight / 100;
+                Console.WriteLine(quote);
+        
+            }
+
+            Console.ReadLine();
+            
+
+        }
+    }
+}
