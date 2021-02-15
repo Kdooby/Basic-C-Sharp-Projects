d5e73dcfb494ac63575d05f534af4058a08c7189
Kdooby <kevinmdem@gmail.com>
2021-02-15 15:31:21 -0800

    Class and Methods assignment
    Simple math utilizing a Class and 3 different methods

---
+++ ClassAndMethods/ClassAndMethods/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to see some basic math: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            simpleMath answer = new simpleMath();
+            int x = answer.addition(input, 25);
+            int y = answer.division(input, 4);
+            int z = answer.modulus(input, 2);
+            
+            Console.WriteLine(input + " plus 25 equals: " + x);
+            Console.ReadLine();
+            
+            Console.WriteLine(input + " divided by 4 equals: " + y);
+            Console.ReadLine();
+            
+            Console.WriteLine(input + " divided by 2, has the remainder of " + z);
+            Console.ReadLine();
+            
+
+        }
+    }
+}
---
+++ ClassAndMethods/ClassAndMethods/simpleMath.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethods
+{
+    public class simpleMath
+    {
+        public int addition(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int division(int x, int y)
+        {
+            return x / y;
+        }
+
+        public int modulus(int x, int y)
+        {
+            return x % y;
+        }
+    }
+}
