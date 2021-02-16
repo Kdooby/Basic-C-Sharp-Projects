14ef3732225b4460c0b08274ac80c05d77e9f9b7
Kdooby <kevinmdem@gmail.com>
2021-02-16 13:05:09 -0800

    ClassAndMethods-5
    void method outputs integer.  And Overload a method.

---
+++ ClassAndMethods-5/ClassAndMethods-5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethods_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mathClass n = new mathClass();
+
+            Console.WriteLine("Enter a Number: ");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+
+            n.DivideBy(input1, out int quotient);
+            Console.ReadLine();
+
+            // Overloaded Class
+
+            mathClass d = new mathClass();
+            Console.WriteLine("Enter a Decimal: ");
+            int input2 = Convert.ToInt32(Console.ReadLine());
+
+            d.DivideBy(input2, quotient: out int total);
+            Console.ReadLine();
+
+
+        }
+       
+        
+    }
+}   
+    
---
+++ ClassAndMethods-5/ClassAndMethods-5/mathClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethods_5
+{
+    public class mathClass
+    {
+        public void DivideBy(int x, out int quotient)
+        {
+            quotient = x / 2;
+            Console.WriteLine("The answer is: " + quotient);
+
+
+        }
+
+        public void DivideBy(decimal x, out decimal quotient)
+        {
+            quotient = x / 2;
+            Console.WriteLine("The answer is: " + quotient);
+        }
+
+    }
+
+
+    static class Class2
+    {
+        public static void Method()
+        {
+            Console.WriteLine("Testing...Testing...");
+        }
+    }
+
+
+}
