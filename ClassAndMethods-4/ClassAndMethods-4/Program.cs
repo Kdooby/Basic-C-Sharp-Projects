ab9074e38130e029e4d7d17c8b5f6c870e160507
Kdooby <kevinmdem@gmail.com>
2021-02-15 19:37:34 -0800

    ClassAndMethods-4
    Same method passing in two numbers, as well as specifying the parameters by name

---
+++ ClassAndMethods-4/ClassAndMethods-4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethods_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mathClass n = new mathClass();
+            
+            Console.WriteLine("Enter a number here: ");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Enter another number here: ");
+            int input2 = Convert.ToInt32(Console.ReadLine());
+            
+            n.method(input1, input2);
+            Console.ReadLine();
+
+
+            Console.WriteLine("Using Parameter Names: ");
+            Console.ReadLine();
+
+            n.method(num1: input1, num2: input2);
+            Console.ReadLine();
+
+
+
+        }    
+    }
+}
---
+++ ClassAndMethods-4/ClassAndMethods-4/mathClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethods_4
+{
+    public class mathClass
+    {
+        public void method(int num1, int num2)
+        {
+            int multiplied = num1 * num2;
+            Console.WriteLine("The answer is: " + multiplied);
+        }
+        
+
+    }
+}
