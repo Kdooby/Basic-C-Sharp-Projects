64d8a61a169456f94fd8d5aa02572b212b40505a
Kdooby <kevinmdem@gmail.com>
2021-02-15 17:38:54 -0800

    Class and Methods drill 2
    Utilize the same method from a class for multiple uses.

---
+++ ClassAndMethods-2/ClassAndMethods-2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethods_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            mathClass method1 = new mathClass();
+
+
+
+            Console.WriteLine("This method takes in an integer and adds 25: {0} ",  method1.same_method(10));
+            Console.ReadLine();
+
+            Console.WriteLine("This method takes in a decimal and returns an int: {0} ", method1.same_method(3.25m));
+            Console.ReadLine();
+
+            Console.WriteLine("This method takes in a string and returns an int, and then multiplies by 5: {0} ", method1.same_method("10"));
+            Console.ReadLine();
+        }
+
+
+    }
+}
---
+++ ClassAndMethods-2/ClassAndMethods-2/mathClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethods_2
+{
+    public class mathClass
+    {
+        public int same_method(int num)
+        {
+           int result = num + 25;
+           return result;
+        }
+
+        public int same_method(decimal num)
+        {
+            int result = Convert.ToInt32(num);
+            return result;
+        }
+
+        public int same_method(string num)
+        {
+
+            int result = Convert.ToInt32(num) * 5;
+            return result;
+
+        }
+    }
+}
