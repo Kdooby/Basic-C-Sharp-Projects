947cc9e8bb9c76ac88f2695231dd2b0e09060316
Kdooby <kevinmdem@gmail.com>
2021-02-15 18:10:42 -0800

    Class and Methods-3
    Create a class method with two parameters, one of which being optional.

---
+++ ClassAndMethods-3/ClassAndMethods-3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethods_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("If you would like, enter a second number: ");
+            string y = Console.ReadLine();
+            bool optional = string.IsNullOrEmpty(y); //if second number is null or empty
+
+            if (optional == true)
+            {
+                // if no input on second number, run this code
+                mathClass n = new mathClass();
+                int i = n.Optional(input);
+                Console.WriteLine("You have a total of: {0}", i);
+            }
+
+            else 
+            {
+                // if user inputs a second number, run this code which utilizes the first number as well. 
+                mathClass n = new mathClass();
+                int i = n.Optional(input, Convert.ToInt32(y)); // turns string into int
+                Console.WriteLine("You have a total of: {0}", i);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ClassAndMethods-3/ClassAndMethods-3/mathClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethods_3
+{
+    public class mathClass
+    {
+        public int Optional(int x = 1, int y = 1)
+        {
+            return x * y;
+
+        }
+    }
+}
