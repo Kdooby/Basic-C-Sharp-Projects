e28d587a5bcd209a7d0025629e57696ab590ad87
Kdooby <kevinmdem@gmail.com>
2021-02-17 15:11:17 -0800

    Abstract
    Basic use of Abstract Classes and Abstract Methods

---
+++ Abstract/Abstract/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    public abstract class Person
+    {
+        // Properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        
+        
+        // Methods
+        public virtual void SayName()
+        {
+            
+
+        }
+    }
+
+    
+
+}
