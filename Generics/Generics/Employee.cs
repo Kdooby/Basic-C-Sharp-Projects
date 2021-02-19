8488448818ae1636d3f404e7013432c67e816726
Kdooby <kevinmdem@gmail.com>
2021-02-18 19:29:14 -0800

    Generics
    Create a generic List, and from that create new string and int list respectively.

---
+++ Generics/Generics/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class Employee<T> // T representing the generic type parameter
+    {
+        public List<T> things { get; set; } // T representing the generic data type in the List, which matches the class parameter
+    
+        public void listOfThings()
+        {
+            foreach (T thing in things)
+            {
+                Console.WriteLine(String.Join("\n, ", thing)); // Prints everything in the list each separated by a new line
+            }
+            
+        }
+        
+    }
+
+
+}
---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employee = new Employee<string>();
+            employee.things = new List<string>() { "Kevin", "Makenna", "Bear", "Bob" };
+
+            Employee<int> employeeNum = new Employee<int>();
+            employeeNum.things = new List<int>() { 4, 3, 0, 5 };
+
+            employee.listOfThings();
+            employeeNum.listOfThings();
+            Console.ReadLine();
+        }
+    }
+}
