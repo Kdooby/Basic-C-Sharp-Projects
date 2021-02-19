76e08c7d164b7804450bc453c0580680d90997e1
Kdooby <kevinmdem@gmail.com>
2021-02-19 00:15:37 -0800

    Structures
    Basic code to show the use of the value type "structs"

---
+++ Structures/Structures/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number cost = new Number();
+
+            cost.Amount = 10.99m;
+            Console.WriteLine("This item costs: " + cost.Amount);
+            Console.ReadLine();
+        
+        }
+
+
+        public struct Number
+        {
+            public decimal Amount { get; set; }
+
+        }
+    }
+    
+}
