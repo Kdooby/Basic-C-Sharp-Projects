556c82227f947aa251b1858a0cf73ee14736bbcf
Kdooby <kevinmdem@gmail.com>
2021-02-18 20:57:04 -0800

    Enums
    Basic code for assigning Enums and parsing through using Try/Catch

---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                Console.WriteLine("Type in the current day of the week: ");
+                string today = Console.ReadLine(); 
+                DaysOfWeek input = (DaysOfWeek)Enum.Parse(typeof(DaysOfWeek), today);
+                
+                {
+                    Console.WriteLine("It is {0} today!", today);
+                }
+                
+            } 
+            catch (FormatException)
+            {
+               
+                Console.WriteLine("Please enter an actual day of the week...");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Please enter an actual day of the week...");
+            }
+            Console.ReadLine();
+        }
+
+        
+        public enum DaysOfWeek
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        }
+
+        
+    }
+}
