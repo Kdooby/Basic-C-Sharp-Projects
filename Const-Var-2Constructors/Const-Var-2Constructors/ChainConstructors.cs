8a3e02bd6597a1dc8b49110fced7ab7d72c19333
Kdooby <kevinmdem@gmail.com>
2021-02-21 13:21:49 -0800

    Constant Var and Chains

---
+++ Const-Var-2Constructors/Const-Var-2Constructors/ChainConstructors.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Const_Var_Chain
+{
+    public class ChainConstructors
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ChainConstructors(int id, string name)
+        {
+            Id = id;
+            Name = name;
+            Console.WriteLine("My ID is: {0} \nMy Name is: {1}", id, name);
+            
+        }
+        
+        public ChainConstructors(string name) : this(101, name)
+        {
+           
+        }
+        
+    }
+}
---
+++ Const-Var-2Constructors/Const-Var-2Constructors/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Const_Var_Chain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string example = "This is an example of a constant";
+            Console.WriteLine(example);
+            Console.ReadLine();
+
+            string x = "This is an example of ";
+            string y = "using the keyword \"var\". ";
+            var z = x + y;
+            Console.WriteLine(z);
+            Console.ReadLine();
+
+
+            var i = new ChainConstructors("Kevin");
+            
+            Console.ReadLine();
+
+
+        }
+    }
+}
