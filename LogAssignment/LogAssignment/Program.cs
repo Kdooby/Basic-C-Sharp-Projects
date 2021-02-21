3d0519b07280ccb43a834c10eeb6c00711b6a5bb
Kdooby <kevinmdem@gmail.com>
2021-02-20 20:10:16 -0800

    Added DateTime functionality

--- LogAssignment/LogAssignment/Program.cs
+++ LogAssignment/LogAssignment/Program.cs
@@ -17,11 +17,27 @@ namespace LogAssignment
             using (StreamWriter file = new StreamWriter(@"C:\Users\kevin\Desktop\Logs\log.txt")) //true will append to file otherwise will create new file which overwrites the previous input
             {
                 file.WriteLine(input);
+                
             }
 
             string text = File.ReadAllText(@"C:\Users\kevin\Desktop\Logs\log.txt");
             Console.WriteLine("\nThis is the number you inputed: {0} ", text);
+            
             Console.ReadLine();
+
+
+            //DATETIME
+
+            Console.Write("Today's Date and Time is " + DateTime.Now);
+            Console.Write("\nPlease enter another number here: ");
+            int input2 = Convert.ToInt32(Console.ReadLine());
+            DateTime now = DateTime.Now;
+            TimeSpan duration = new TimeSpan(input2, 0, 0);  //input2 = hours, 0 = minutes, 0 = seconds
+            DateTime answer = now.Add(duration);
+            Console.WriteLine(input2 + " hours from now it will be " + answer);
+            Console.ReadLine();
+
+            
         }
     }
 }
