065bb9905af6cd0379cd83fdedcb09d691380f3b
Kdooby <kevinmdem@gmail.com>
2021-02-20 19:20:03 -0800

    Log Assignment
    Example how to save data to a log and the read the data back to you.

---
+++ LogAssignment/LogAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LogAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a number here: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            using (StreamWriter file = new StreamWriter(@"C:\Users\kevin\Desktop\Logs\log.txt")) //true will append to file otherwise will create new file which overwrites the previous input
+            {
+                file.WriteLine(input);
+            }
+
+            string text = File.ReadAllText(@"C:\Users\kevin\Desktop\Logs\log.txt");
+            Console.WriteLine("\nThis is the number you inputed: {0} ", text);
+            Console.ReadLine();
+        }
+    }
+}
